15f28a3d3c45e073f20cd31154b353a17aa9c4f8
José-Carlos Aguilar-Paz <mail@josecarlos.de>
2020-06-14 18:46:18 +0200

    wip: start project with netlify identity

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Index = () => {
+
+  return (
+    <>
+      <h1>Sign up for Premium Content</h1>
+      <p>Get your subsription today!</p>
+    </>
+  );
+};
+
+export default Index;
